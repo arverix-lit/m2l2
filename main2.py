61b8e4498b4184dc89d9bb9fdd6af2dacdbfafa2
arverix-lit <aisyahhc1202@gmail.com>
2025-06-22 17:16:07 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import os, random
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+@bot.command()
+async def mem(ctx):
+    with open('images/mem1.jpeg', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+@bot.command('solusi')
+async def solusi(ctx):
+    with open('solusi.txt', 'r', encoding='utf-8') as f:
+        await ctx.send(f.read())
+
+bot.run("")
