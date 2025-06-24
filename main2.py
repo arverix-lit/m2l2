ac12e04668ffa479612c969d8deb6fb326f7f3e2
arverix-lit <aisyahhc1202@gmail.com>
2025-06-24 15:18:11 +0700

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import os, random
-
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -27,14 +27,31 @@ async def repeat(ctx, times: int, content='repeating...'):
 @bot.command()
 async def mem(ctx):
     with open('images/mem1.jpeg', 'rb') as f:
-        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
         picture = discord.File(f)
-   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+
     await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 @bot.command('solusi')
 async def solusi(ctx):
     with open('solusi.txt', 'r', encoding='utf-8') as f:
+        await ctx.send(f.read())
+
+bot.command('tutorial')
+async def tutorial(ctx):
+    with open('tutorial.txt', 'r', encoding='utf-8') as f:
         await ctx.send(f.read())
 
 bot.run("")
